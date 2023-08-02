fdd9dc1ccb20bc7b741278952fe231dea8d4354d
mike.garbuzenko@gmail.com <mike.garbuzenko@gmail.com>
2023-08-02 22:24:29 +0300

    Update health.js

--- src/js/health.js
+++ src/js/health.js
@@ -1,5 +1,10 @@
 export default function showHealth(units) {
   let result;
+
+  // if (Number.isNaN(units.health) || units.health === undefined || units.health < 0 || typeof (units.health) !== 'number') {
+  //   throw new Error('Parameter is not a number!');
+  // }
+
   if (units.health > 50) {
     result = 'healthy';
   }
@@ -9,8 +14,6 @@ export default function showHealth(units) {
   if (units.health <= 15 && units.health > 0) {
     result = 'critical';
   }
-  if (Number.isNaN(units.health) || units.health === undefined || units.health < 0 || typeof (units.health) !== 'number') {
-    throw new Error('Parameter is not a number!');
-  }
+
   return result;
 }
